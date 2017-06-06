b31388046c9bc92d1eb853d73a565dc92e9d8f3e
RyodoTanaka <k104073r@mail.kyutech.jp>
2017-06-06 15:48:35 +0900

    [modify] make be able to treat big stories

--- python/0/0.6.8.py
+++ python/0/0.6.8.py
@@ -2,7 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import inverse_index as ii
+
+print "==== Small Stories ===="
 f = open('stories_small.txt')
 stories = list(f)
 iindex = ii.makeInverseIndex(stories)
 print ii.andSearch(iindex, ['prep', 'course'])
+
+print "==== Big Stories ===="
+f = open('stories_big.txt')
+stories = list(f)
+iindex = ii.makeInverseIndex(stories)
+print ii.andSearch(iindex, ['the', 'all', 'his'])
