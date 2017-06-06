f8d4358350be5725630e769e613efd9455c3966c
RyodoTanaka <k104073r@mail.kyutech.jp>
2017-06-06 10:48:57 +0900

    [rename] 0.5.26 to 0.5.25

--- python/0/0.5.25.py
+++ python/0/0.5.25.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-D={'red','white','blue'}
+base=10
+digits=set(range(base))
 
-ret={ i:i for i in D}
+ret = {k:(k/(base**2)%base,k/(base)%base,k%base) for k in range(1000)}
+print ret
+
+base=2
+ret = {k:(k/(base**2)%base,k/(base)%base,k%base) for k in range(8)}
 print ret
