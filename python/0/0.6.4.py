3e9a0f5af0c046fa2f609b04e587645ed4eeab21
RyodoTanaka <k104073r@mail.kyutech.jp>
2017-06-06 14:53:01 +0900

    [add] 0.6.4

---
+++ python/0/0.6.4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import dictutil
+
+dct={'a':'A', 'b':'B', 'c':'C'}
+keylist=['b', 'c', 'a']
+print dictutil.dict2list(dct, keylist)
+
+L=['A', 'B', 'C']
+keylist=['a', 'b', 'c']
+print dictutil.list2dict(L, keylist)
+
+L=['A', 'B', 'C']
+print dictutil.list2dict2(L)
