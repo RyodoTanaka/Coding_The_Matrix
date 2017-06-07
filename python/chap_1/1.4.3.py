43d6029455095cd2ea00536d5fcfc8724445d28e
RyodoTanaka <k104073r@mail.kyutech.jp>
2017-06-07 13:48:35 +0900

    [modify] change cplotting lib

--- python/chap_1/1.4.3.py
+++ python/chap_1/1.4.3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from cplotting import plot
+import cplotting as cplot
 
 S={2+2j, 3+2j, 1.75+1j, 2+1j, 2.25+1j, 2.5+1j, 2.75+1j, 3+1j, 3.25+1j}
 
-plot({1+2j+z for z in S},4)
+cplot.plot({1+2j+z for z in S},4)
+cplot.show()
