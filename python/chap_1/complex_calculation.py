ae3c01c31b7b286f5c3479e621153adf08bd12ca
RyodoTanaka <k104073r@mail.kyutech.jp>
2017-06-07 17:54:55 +0900

    [add] center function

--- python/chap_1/complex_calculation.py
+++ python/chap_1/complex_calculation.py
@@ -4,4 +4,26 @@
 import math as m
 
 def rotate(S, rotate):
-    return [abs(z)*m.e**( ( m.atan(float(z.imag)/float(z.real)) + rotate ) * 1j ) if z.real!=0 else abs(z)*m.e**(rotate*1j) for z in S]
+    ret=[]
+    theta=float()
+    
+    for z in S:
+        if z.real>0:
+            theta=m.atan(float(z.imag)/float(z.real))
+        elif z.real<0:
+            theta=m.atan(float(z.imag)/float(z.real))+m.pi
+        else:
+            theta=m.pi if z.imag > 0 else 3./2.*m.pi
+        ret.append( abs(z)*m.e**((theta+rotate)*1j) )
+    
+    return ret
+    
+
+def make_center(S):
+    r = [z.real for z in S]
+    c = [z.imag for z in S]
+
+    r_center = (min(r)+max(r))/2.
+    c_center = (min(c)+max(c))/2.
+
+    return [z.real-r_center + (z.imag-c_center)*1j for z in S]
