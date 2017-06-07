0b87a66d430b12c4d8e5b5789a7a0ebad5c07317
RyodoTanaka <k104073r@mail.kyutech.jp>
2017-06-07 15:33:28 +0900

    nothing is modified

--- python/chap_1/1.4.17.py
+++ python/chap_1/1.4.17.py
@@ -5,6 +5,6 @@ import math as m
 import cplotting as cplot
 
 n=20
-w=[m.e**(2*m.pi*1j/float(k)) for k in range(1,n+1)]
+w=[m.e**(2.*m.pi*1j/float(k)) for k in range(1,n+1)]
 cplot.plot({z for z in w},4)
 cplot.show()
