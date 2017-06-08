e5bb91ced78cb00fec14c5b4b9140ce095799036
RyodoTanaka <k104073r@mail.kyutech.jp>
2017-06-08 13:25:07 +0900

    [modify] change plotting lib

--- python/chap_2/2.3.2.py
+++ python/chap_2/2.3.2.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import vector_plot as vplt
+import cplotting as cplt
 
 L=[ [2,2],[3,2],[1.75,1],[2,1],[2.25,1],[2.5,1],[2.75,1],[3,1],[3.25,1] ]
 limit=4
 
-vplt.plot2d(L, limit)
-vplt.show()
-
+cplt.plot(L, limit)
+cplt.show()
